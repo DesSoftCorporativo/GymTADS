650e5bec21fd7ddc903cdd6a255d722d54551dd2
gabri <gabri@192.168.120.224>
2022-10-31 19:45:41 -0300

    Segundo

---
+++ GymTADS/src/main/java/com/ifpe/edu/br/gymtads/model/Aluno.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ifpe.edu.br.gymtads.model;
+
+/**
+ *
+ * @author gabri
+ */
+public class Aluno extends Usuario{
+    private Personal personal;
+    private Treino treino;   
+
+    public Personal getPersonal() {
+        return personal;
+    }
+
+    public void setPersonal(Personal personal) {
+        this.personal = personal;
+    }
+
+    public Treino getTreino() {
+        return treino;
+    }
+
+    public void setTreino(Treino treino) {
+        this.treino = treino;
+    }
+    
+}
---
+++ GymTADS/src/main/java/com/ifpe/edu/br/gymtads/model/Usuario.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ifpe.edu.br.gymtads.model;
+
+/**
+ *
+ * @author gabri
+ */
+import java.util.Date;
+
+/**
+ *
+ * @author gabri
+ */
+public abstract class Usuario {
+    private long id;
+    private String nome;
+    private String sobrenome;
+    private String cpf;
+    private String endereco;
+    private String telefone;
+    private Date nascimento;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Date getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(Date nascimento) {
+        this.nascimento = nascimento;
+    }
+}
